5a907e77c6f1f1b6bbadd0c9ea1fa814865a25a3
Dustin Filippini <dusty@dustyf.com>
2013-11-23 12:47:29 -0600

    Commit initial plugin files

---
+++ trico-geolocate-ip.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: IP Geolocation Check
+Plugin URI: http://dustyf.com
+Description: Finds the user's IP address and checks the geolocation of it.
+Author: Dustin Filippini
+Version: 0.1
+Author URI: http://dustyf.com
+*/
+
+/**
+ * Find the IP of the visitor
+ */
+function what_is_client_ip() {
+    $ipaddress = '';
+    if ( $_SERVER['HTTP_CLIENT_IP'] ) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if ( $_SERVER['HTTP_X_FORWARDED_FOR'] ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if ( $_SERVER['HTTP_X_FORWARDED'] ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if ( $_SERVER['HTTP_FORWARDED_FOR'] ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if ( $_SERVER['HTTP_FORWARDED'] ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if ( $_SERVER['REMOTE_ADDR'] ) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+ 
+    return $ipaddress;
+}
+
+/**
+ * A conditional to find if the IP Address is from the US
+ */
+function is_us(){
+	if ( ! isset( $_SESSION['country'] ) ) {
+		$ipaddress = what_is_client_ip();
+		$url = 'http://freegeoip.net/json/' . $ipaddress;
+		$response = wp_remote_get( $url );
+		$body = json_decode( $response['body'] );
+		$country = $body->country_code;
+		$_SESSION['country'] = $country;
+	} 
+
+	if ( 'US' !== $_SESSION['country'] ) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/**
+ * A conditionaly that allows you to pass a country code
+ * to check if the IP address is from there.
+ */
+function is_country( $country_code ){
+	if ( ! isset( $_SESSION['country'] ) ) {
+		$ipaddress = what_is_client_ip();
+		$url = 'http://freegeoip.net/json/' . $ipaddress;
+		$response = wp_remote_get( $url );
+		$body = json_decode( $response['body'] );
+		$country = $body->country_code;
+		$_SESSION['country'] = $country;
+	} 
+
+	if ( $country_code !== $_SESSION['country'] ) {
+		return false;
+	} else {
+		return true;
+	}
+}
