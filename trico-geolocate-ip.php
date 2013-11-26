1b81e4a1417834d9b35fcede4f1011c897257331
Faison Zutavern <fzutavern@my.wctc.edu>
2013-11-26 14:24:01 -0600

    Added isset() function calls to what_is_client_ip() to eliminate Debug Mode notices.

--- trico-geolocate-ip.php
+++ trico-geolocate-ip.php
@@ -13,17 +13,17 @@ Author URI: http://dustyf.com
  */
 function what_is_client_ip() {
     $ipaddress = '';
-    if ( $_SERVER['HTTP_CLIENT_IP'] ) {
+    if ( isset( $_SERVER['HTTP_CLIENT_IP'] ) $_SERVER['HTTP_CLIENT_IP'] ) {
         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    } else if ( $_SERVER['HTTP_X_FORWARDED_FOR'] ) {
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) $_SERVER['HTTP_X_FORWARDED_FOR'] ) {
         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else if ( $_SERVER['HTTP_X_FORWARDED'] ) {
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED'] ) $_SERVER['HTTP_X_FORWARDED'] ) {
         $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    } else if ( $_SERVER['HTTP_FORWARDED_FOR'] ) {
+    } else if ( isset( $_SERVER['HTTP_FORWARDED_FOR'] ) $_SERVER['HTTP_FORWARDED_FOR'] ) {
         $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    } else if ( $_SERVER['HTTP_FORWARDED'] ) {
+    } else if ( isset( $_SERVER['HTTP_FORWARDED'] ) $_SERVER['HTTP_FORWARDED'] ) {
         $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    } else if ( $_SERVER['REMOTE_ADDR'] ) {
+    } else if ( isset( $_SERVER['REMOTE_ADDR'] ) $_SERVER['REMOTE_ADDR'] ) {
         $ipaddress = $_SERVER['REMOTE_ADDR'];
     } else {
         $ipaddress = 'UNKNOWN';
